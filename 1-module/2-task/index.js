96798a20c0ea9a169f460ef6c46ff474474d5c59
SafinSI <37666689+SafinSI@users.noreply.github.com>
2022-04-19 02:52:30 +0500

    Update index.js

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,10 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name && name.length > 3 && name.indexOf(' ') === -1) {
+    return true
+  }
+  return false
 }
 
 function sayHello() {
