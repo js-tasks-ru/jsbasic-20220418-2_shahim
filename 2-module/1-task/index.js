2c95cf4565bee990825f4619f40cce027bb9fd3f
shahim <safinsi@mail.ru>
2022-04-23 12:34:50 +0500

    module 2 task 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
+function isValidNumber(item) {
+  // Либо можно использовать функцию Number.isFinite(item)
+  return !isNaN(item) && typeof item === 'number' && item !== (item + 1);
+}
+
+
 function sumSalary(salaries) {
-  // ваш код...
+  let resultSalary = 0;
+  Object.values(salaries).forEach(item => {
+    if (isValidNumber(item)) {
+      resultSalary += item;
+    }
+  });
+  return resultSalary;
 }
