07507a911f02c0c152002a81834067bb62464b87
shahim <safinsi@mail.ru>
2022-04-23 12:57:28 +0500

    module 2 task 1-3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  x: 0,
+  y: 0,
+  read: function(x, y) {
+    this.x = x;
+    this.y = y;
+  },
+  sum: function() {
+    return this.x + this.y;
+  },
+  mul: function() {
+    return this.x * this.y;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
