978a938eceb4022585764901047a7f06038547d8
shahim <safinsi@mail.ru>
2022-04-27 23:46:37 +0500

    module 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let usersInfo = users.reduce(function(balanceInfo, user) {
+    if (user.age <= age) {
+      return balanceInfo + user.name + ', ' + user.balance + '\n';
+    }
+    return balanceInfo;
+  }, '');
+  return usersInfo.slice(0, -1);
 }
