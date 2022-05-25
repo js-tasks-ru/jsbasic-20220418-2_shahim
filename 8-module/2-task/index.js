30e015fa414cbcd138d9c182ac8eb2b3a1b4a856
shahim <safinsi@mail.ru>
2022-05-25 14:11:19 +0500

    module 8

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -2,8 +2,76 @@ import createElement from '../../assets/lib/create-element.js';
 import ProductCard from '../../6-module/2-task/index.js';
 
 export default class ProductGrid {
+  #elem = '';
+  #prevFilters = {};
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
   }
+
+  get elem() {
+    return this.#elem;
+  }
+
+  render() {
+    const template = createElement(`
+    <div class="products-grid">
+      <div class="products-grid__inner">
+        <!--ВОТ ТУТ БУДУТ КАРТОЧКИ ТОВАРОВ-->
+      </div>
+    </div>`);
+    template.querySelector('.products-grid__inner').append(...this.products.map(item => {
+      const productCard = new ProductCard(item);
+      return productCard.elem;
+    }));
+    this.#elem = template;
+  }
+
+  filtrator = (product, properties) => {
+    return Object.keys(properties).reduce((result, key) => {
+      if (!properties[key]) {
+        return result;
+      }
+      if (key === 'noNuts') {
+        if (product.nuts) {
+          result = false;
+          return result;
+        }
+      }
+      if (key === 'vegeterianOnly') {
+        if (!product.vegeterian) {
+          result = false;
+          return result;
+        }
+      }
+      if (key === 'category') {
+        if (product.category !== properties[key]) {
+          result = false;
+          return result;
+        }
+      } 
+      if (key === 'maxSpiciness') {
+        if (product.spiciness > properties[key]) {
+          result = false;
+          return result;
+        }
+      }
+      return result;
+    }, true);
+  }
+
+  updateFilter(filters) {
+    // дополняем критериями фильтрации после предыдущего вызова
+    let properties = Object.assign(this.#prevFilters, filters); 
+    const filteredProducts = this.products.filter(product => this.filtrator(product, properties));
+    this.#elem.querySelector('.products-grid__inner').innerHTML = '';
+    this.#elem.querySelector('.products-grid__inner').append(...filteredProducts.map(item => {
+      const productCard = new ProductCard(item);
+      return productCard.elem;
+    }));
+    // запоминаем критерии фильрации
+    this.#prevFilters = properties;
+  }
+
 }
--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,48 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    const productId = product?.id;
+    if (productId) {
+      const ind = this.cartItems.findIndex(item => item.product.id === productId);
+      if (ind === -1) {
+        const cartItem = { 
+          product: product,
+          count: 1
+        };
+        this.cartItems.push(cartItem);
+        this.onProductUpdate(cartItem);
+      } else {
+        this.cartItems[ind].count += 1;
+        this.onProductUpdate(this.cartItems[ind]);
+      }
+    }
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const ind = this.cartItems.findIndex(item => item.product.id === productId);
+    this.cartItems[ind].count += amount;
+    if (this.cartItems[ind].count === 0) {
+      this.cartItems.splice(ind, 1);
+    }
+    this.onProductUpdate(this.cartItems[ind]);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length < 1;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((acc, item) => {
+      acc += item.count;
+      return acc;
+    }, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((acc, item) => {
+      acc += item.product.price * item.count;
+      return acc;
+    }, 0);
   }
 
   onProductUpdate(cartItem) {
